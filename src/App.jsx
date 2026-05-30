ec10b0575fdbd14b3c4df857d68ebc207bd55469
Lakhwinder-bit <singhlakhiwinder3@gmail.com>
2026-05-30 17:12:07 +0530

    add the final code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./routes/appRoutes";
 export default function app(){
   return(
     <>
