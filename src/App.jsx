a22419d1d98a72552c8e7cf000b61938696ffd09
Lakhwinder-bit <singhlakhiwinder3@gmail.com>
2026-05-30 07:18:55 +0530

    my frist commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function app(){
+  return(
+    <>
+    <div>
+      <h1 className="text-5xl text-violet-600 font-bold">This is main page</h1>
+    </div>
+    </>
+  )
+}
